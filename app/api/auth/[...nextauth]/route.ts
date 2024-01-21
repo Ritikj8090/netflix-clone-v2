1f54670553e61af583b7ba22bebdd51839cb7aa5
Ritikj8090 <ritikj713@gmail.com>
2024-01-21 19:57:28 +0530

    ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { compare } from 'bcrypt';
 import prismadb from '@/libs/prismadb';
   
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prismadb),
   providers: [
     GithubProvider({
@@ -59,5 +59,6 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET
 };
 
-export const GET = handler.handlers.GET;
-export const POST = handler.handlers.POST;
+const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST}
